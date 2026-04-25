b05c1b7120426e125ac98c8bbcbda0c2830bc1da
ymty-portfolio <ymty.007@gmail.com>
2026-04-25 15:26:59 +0900

    Add files via upload

--- onsenyado_site/js/script.js
+++ onsenyado_site/js/script.js
@@ -8,11 +8,11 @@ $(function () {
             $(".top-btn").show();
             return;
         }
-    
+
         let scT = $(window).scrollTop();
         let footer = $("footer").offset().top;
         let winH = $(window).height();
-    
+
         if (scT >= (footer - (winH - 200))) {
             $(".top-btn").css("display", "none");
             $(".up-btn").css("display", "block");
@@ -20,13 +20,13 @@ $(function () {
             $(".top-btn").css("display", "block");
             $(".up-btn").css("display", "none");
         }
-    
+
     });
     // スクロールフワ
     $(window).scroll(function () {
         let scT = $(window).scrollTop();
         $(".fuwa").each(function () {
-            if (scT > $(this).offset().top-$(window).height()) {
+            if (scT > $(this).offset().top - $(window).height()) {
                 $(this).addClass("on");
             } else {
                 $(this).removeClass("on");
@@ -36,49 +36,49 @@ $(function () {
 
     //    ハンバーガーメニュー
     $(".hum-menu").click(function () {
-        $(this).toggleClass("on");
-        $("nav").toggleClass("on");
+        $(this).toggleClass("open");
+        $("nav").toggleClass("open");
     });
-    
+
     // スライダー
-            $('.slick').slick({
-                autoplay:true,
-                autoplaySpeed:5000,
-                speed:2000,
-                slidesToShow:4,
-                arrows:false,
-                responsive:[
-                    {
-                        breakpoint:1000,
-                        settings:{slidesToShow:3}
-                    },
-                    {
-                        breakpoint:700,
-                        settings:{slidesToShow:2}
-                    },
-                    {
-                        breakpoint:500,
-                        settings:{slidesToShow:1}
-                    }
-                ]
-            });
+    $('.slick').slick({
+        autoplay: true,
+        autoplaySpeed: 5000,
+        speed: 2000,
+        slidesToShow: 4,
+        arrows: false,
+        responsive: [
+            {
+                breakpoint: 1000,
+                settings: { slidesToShow: 3 }
+            },
+            {
+                breakpoint: 700,
+                settings: { slidesToShow: 2 }
+            },
+            {
+                breakpoint: 500,
+                settings: { slidesToShow: 1 }
+            }
+        ]
+    });
     $('.form-slide').slick({
         autoplay: true,
-        autoplaySpeed:4000,
-        speed:1500,
+        autoplaySpeed: 4000,
+        speed: 1500,
     });
 
     // レスポンシブ
-    $(window).scroll(function(){
+    $(window).scroll(function () {
 
         let fv = $(".top-img").height();
         let sc = $(window).scrollTop();
-        
-        if(sc > fv){
-        $(".header").addClass("scrolled");
-        }else{
-        $(".header").removeClass("scrolled");
+
+        if (sc > fv) {
+            $(".header").addClass("scrolled");
+        } else {
+            $(".header").removeClass("scrolled");
         }
-        
-        });
+
+    });
 });
